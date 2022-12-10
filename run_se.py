3f9d3bb367c4eb93e2cf4873ece0138f3a84a4be
kassieclaire <59930829+kassieclaire@users.noreply.github.com>
2022-12-09 18:42:47 -0600

    updated python run scripts to treat all args (including binary name) equally

--- run_se.py
+++ run_se.py
@@ -5,24 +5,15 @@ import sys
 import os
 
 #function which runs a binary file with arguments in the gem5 simulator
-def run_se(binary_file_name, args=None):
-    #if no arguments are given, run the binary file with no arguments
-    if args is None:
-        subprocess.run(["docker", "run", "--rm", "-v", f"{os.getcwd()}/gem5-wd:/gem5", "-it", "gem5-dev", "run-program-se", binary_file_name])
-    #if arguments are given, run the binary file with the given arguments
-    else:
-        #print what is being run
-        print(["docker", "run", "--rm", "-v", f"{os.getcwd()}/gem5-wd:/gem5", "-it", "gem5-dev", "run-program-se", binary_file_name, *args])
-        subprocess.run(["docker", "run", "--rm", "-v", f"{os.getcwd()}/gem5-wd:/gem5", "-it", "gem5-dev", "run-program-se", binary_file_name, *args])
+def run_se(args):
+    
+    #print what is being run
+    print(["docker", "run", "--rm", "-v", f"{os.getcwd()}/gem5-wd:/gem5", "-it", "gem5-dev", "run-program-se", *args])
+    subprocess.run(["docker", "run", "--rm", "-v", f"{os.getcwd()}/gem5-wd:/gem5", "-it", "gem5-dev", "run-program-se", *args])
 
 #script which calls run_se with the name of the binary file and the arguments as command line arguments
 if __name__ == "__main__":
-    #take the name of the binary file as a command line argument
-    binary_file_name = sys.argv[1]
-    #take the arguments to pass to the binary file as a command line argument
-    args = sys.argv[2:]
-    #if args is an empty list, set it to None
-    if len(args) == 0:
-        args = None
+    #args is all arguments including the name of the file
+    args = sys.argv[1:]
     #run the binary file
-    run_se(binary_file_name, args)
+    run_se(args)
